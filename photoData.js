ef0487705461b147864ae0a7316baabb99d60bce
道明敬史 <166388033+Doumyou@users.noreply.github.com>
2024-05-26 19:57:19 +0900

    Update photoData.js

--- photoData.js
+++ photoData.js
@@ -31,7 +31,7 @@ function savePhotos() {
 //-------------------------------------------------------------------------
 
 //写真送り
-//ファイル名一覧の取得
+//画像表示htmlの作成
 const img = ["2024050101.jpg",
             "2024050301.jpg",
             "2024050302.jpg",
@@ -43,18 +43,43 @@ const img = ["2024050101.jpg",
             "2024051701.jpg",
             "2024051801.jpg",
         ];
-//写真送り
-let cnt = 0;
-function changePhotoData() {
-    //画像選択
-    // changePhoto.src = "photo/" + img[cnt];     //自宅用
-    changePhoto.src = "https://doumyou.github.io/Doumyou.fnd27/photo/" + img[cnt];     //提出サーバー用
-    cnt++;
-    if (cnt == img.length) {
-        cnt = 0
-    };
-    //秒数の指定(4sで切り替え(変更するときはstyle.cssも変更必要))
-    setTimeout("changePhotoData()", 5000);
+const inputPhotoData = document.getElementById("slidesList");
+let divCode;
+let imgCode;
+for (const photoImg of img) {
+    divCode = document.createElement("div");    //要素divの作成
+    divCode.classList.add("swiper-slide");      //要素divにクラスの設定
+    imgCode = document.createElement("img");    //要素imgの作成
+    imgCode.src = "photo/" + photoImg;          //画像の設定 自宅用
+    // imgCode.src = "https://doumyou.github.io/Doumyou.fnd27/photo/" + photoImg;          //画像の設定 提出サーバー用
+    divCode.appendChild(imgCode);
+    inputPhotoData.appendChild(divCode);      
 }
-changePhotoData();
 
+//swiper.jsの初期設定
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoHeight: true,
+    autoplay: {
+        delay: 3000,
+    },
+    keepAspectRatio: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
